09bf5943e59a1e6d06cabd77ce3ee9274728d005
ellieasona <ellie.a.sona@vanderbilt.edu>
2019-04-29 04:29:26 -0500

    Delete Initiator.py

--- Initiator.py
+++
@@ -1,7 +0,0 @@
-import rospy
-from PrimaryHandler import *
-
-if __name__ == "__main__":
-    rospy.init_node('Demo', anonymous=True)
-    miro1 = SecondaryInterface('rob01', 1, 6.283185307/2)
-    miro1.sensorinterrupt()
